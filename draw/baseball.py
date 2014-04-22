e0767557d5f1201935517f73d17438c52365a555
Charlie Marshall <chas11man@gmail.com>
2014-04-22 15:21:17 -0400

    Even better

--- draw/baseball.py
+++ draw/baseball.py
@@ -23,6 +23,19 @@ def bounding(coord1, coord2):
 def get_font(size):
     return ImageFont.truetype('/usr/share/fonts/truetype/freefont/FreeMono.ttf', size)
 
+def play(scoring, base=1):
+    if base == 1:
+        c = coord_20(16,7)
+    elif base == 2:
+        c = coord_20(12,19)
+    elif base == 3:
+        c = coord_20(1,16)
+    else:
+        c = coord_20(5,3)
+
+    font = get_font(40)
+    draw.text(c, str(scoring), font=font)
+
 def draw_diamond():
     draw.line([coord_10(5,1), coord_10(9,5)], width=2, fill=128) # Home - 1st
     draw.line([coord_10(9,5), coord_10(5,9)], width=2, fill=128) #  1st - 2nd
@@ -39,82 +52,56 @@ def draw_count():
     draw.line([coord_10(0,2), coord_10(2,2)], width=2, fill=64)
     draw.line([coord_10(2,0), coord_10(2,2)], width=2, fill=64)
 
-def home_first():
+def home_first(scoring=None):
     draw.line([coord_10(5,1), coord_10(9,5)], width=8)
+    if scoring:
+        play(scoring, 1)
 
-def first_second():
+def first_second(scoring=None):
     draw.line([coord_10(9,5), coord_10(5,9)], width=8)
+    if scoring:
+        play(scoring, 2)
 
-def second_third():
+def second_third(scoring=None):
     draw.line([coord_10(5,9), coord_10(1,5)], width=8)
+    if scoring:
+        play(scoring, 3)
 
-def third_home():
+def third_home(scoring=None):
     draw.line([coord_10(1,5), coord_10(5,1)], width=8)
+    if scoring:
+        play(scoring, 4)
 
 def score():
     draw.polygon((coord_10(5,1), coord_10(9,5), coord_10(5,9), coord_10(1,5)), fill=100)
 
-def big_out(play, num):
+def big_out(scoring, num):
     font = get_font(80)
-    width = font.getsize(play)[0]/2
-    draw.text((image.size[0]/2-width,image.size[1]*.4), play, font=font)
+    width = font.getsize(scoring)[0]/2
+    draw.text((image.size[0]/2-width,image.size[1]*.4), scoring, font=font)
     out(num)
-
-def play(play, base=1):
-    if base == 1:
-        c = coord_20(16,7)
-    elif base == 2:
-        c = coord_20(14,17)
-    elif base == 3:
-        c = coord_20(3,16)
-    else:
-        c = coord_20(5,3)
-
-    font = get_font(40)
-    draw.text(c, str(play), font=font)
-
-def single():
-    home_first()
-    play('1B', 1)
-
-def double():
-    home_first()
-    first_second()
-    play('2B', 2)
-
-def tripple():
-    home_first()
-    first_second()
-    second_third()
-    play('3B', 3)
-
-def home_run():
-    home_first()
-    first_second()
-    second_third()
-    third_home()
-    score()
-    play('HR', 4)
 
 def out(num):
     font = get_font(80)
     draw.text(coord_20(1,4), str(num), font=font)
 
-def out_at_1st(num):
-    draw.line([coord_20(10,5), coord_20(13,8)], width=6)
-    draw.line([coord_20(12,9), coord_20(14,7)], width=3)
+def out_at_2nd(scoring, num):
+    draw.line([coord_10(9,5), coord_20(13,15)], width=8)
+    draw.line([coord_10(6,7), coord_10(7,8)], width=6)
+    play(scoring, 2)
+    out(num)
 
-def out_at_2nd(num):
-    draw.line([coord_20(15,10), coord_20(12,13)], width=6)
-    draw.line([coord_20(11,12), coord_20(13,14)], width=3)
+def out_at_3rd(scoring, num):
+    draw.line([coord_10(5,9), coord_20(5,13)], width=8)
+    draw.line([coord_10(2,7), coord_10(3,6)], width=6)
+    play(scoring, 3)
+    out(num)
 
-def out_at_3rd(num):
-    draw.line([coord_20(10,15), coord_20(7,12)], width=6)
-    draw.line([coord_20(8,11), coord_20(6,13)], width=3)
-
-def out_at_home(num):
-    draw.line([coord_20(5,10), coord_20(8,7)], width=6)
-    draw.line([coord_20(7,6), coord_20(9,8)], width=3)
+def out_at_home(scoring, num):
+    draw.line([coord_10(1,5), coord_20(7,5)], width=8)
+    draw.line([coord_10(3,2), coord_10(4,3)], width=6)
+    play(scoring, 4)
+    out(num)
 
 def strikes(num):
     for i in xrange(num):
